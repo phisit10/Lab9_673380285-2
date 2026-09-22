f27cc716e7c7aeecef59ce93733077d675b7b6e9
phisit10 <phisit.sap@kkumail.com>
2026-09-22 18:07:29 +0700

    Update DepositService.java

--- src/main/java/com/example/lab9/service/DepositService.java
+++ src/main/java/com/example/lab9/service/DepositService.java
@@ -5,7 +5,7 @@ import com.example.lab9.model.DepositTransaction;
 import com.example.lab9.repository.AccountRepository;
 import com.example.lab9.repository.DepositRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
 public class DepositService {
@@ -18,21 +18,20 @@ public class DepositService {
         this.depositRepository = depositRepository;
     }
 
-    @Transactional
+    //@Transactional
     public void deposit(Long accountId, Double amount) {
 
-        // 1. ค้นหา Account จาก accountId
         Account account = accountRepository.findById(accountId)
-                .orElseThrow(() -> new RuntimeException("Account not found with id: " + accountId));
+                .orElseThrow(() -> new RuntimeException("Account not found: " + accountId));
 
-        // 2. เพิ่มจำนวนเงินเข้า balance แล้วบันทึก Account
         account.setBalance(account.getBalance() + amount);
         accountRepository.save(account);
 
-        // 3. สร้าง DepositTransaction ผูกกับ Account นั้น แล้วบันทึกลง Database
-        DepositTransaction transaction = new DepositTransaction(amount, account);
-        depositRepository.save(transaction);
+        DepositTransaction deposit = new DepositTransaction();
+        deposit.setAmount(amount);
+        deposit.setAccount(account);
+        depositRepository.save(deposit);
 
-        // จุดนี้ใช้ทดลอง Rollback ในข้อ 12: throw new RuntimeException("Test Rollback");
+       // throw new RuntimeException("Test Rollback");
     }
 }
